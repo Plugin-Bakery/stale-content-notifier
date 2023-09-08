04e86e2cd63dc7b72b1298d24710a97ac2187130
Alen Ribic <alen.ribic@gmail.com>
2023-09-07 17:28:24 -0700

    Desc tweka and add cache file to gitignore.

--- stale-content-notifier.php
+++ stale-content-notifier.php
@@ -2,7 +2,7 @@
 
 /**
  * Plugin Name:     Stale Content Notifier
- * Description:     WP plugin to keep content up to date by receiving notifications regarding stale content
+ * Description:     Keep content up to date by notifing admin users of stale content
  * Plugin URI:      https://plugin-bakery.com/stale-content-notifier
  * Author:          Alen Ribic
  * Author URI:      https://plugin-bakery.com
